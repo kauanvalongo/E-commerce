53be8191b3cd7f7919f02b3b5ec82af709f1b9d4
kauanvalongo <116652279+kauanvalongo@users.noreply.github.com>
2023-01-24 09:57:52 -0300

    working with sessions

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
 def cart_home(request):
+    request.session['cart_id'] = 123
+    request.session['user'] = request.user.username
     return render(request, "carts/home.html", {} )
-    
