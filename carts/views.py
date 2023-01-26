4f3aec64c65e87d2d5e9125f637c1777f780225e
kauanvalongo <116652279+kauanvalongo@users.noreply.github.com>
2023-01-26 11:30:35 -0300

    update "carts\views.py"

--- carts/views.py
+++ carts/views.py
@@ -16,8 +16,11 @@ def cart_update(request):
     product_obj = Product.objects.get(id=product_id)
     # Cria ou pega a instância já existente do carrinho
     cart_obj, new_obj = Cart.objects.new_or_get(request)
-    # E o produto se adiciona a instância do campo M2M 
-    cart_obj.products.add(product_obj) # cart_obj.products.add(product_id)
-    #cart_obj.products.remove(product_obj) # cart_obj.products.remove(product_id)
-    #return redirect(product_obj.get_absolute_url())
+    if product_obj in cart_obj.products.all():
+        cart_obj.products.remove(product_obj) # cart_obj.products.remove(product_id)
+    else:
+        # E o produto se adiciona a instância do campo M2M 
+        cart_obj.products.add(product_obj) # cart_obj.products.add(product_id)
+    
+    # retorna redirect(product_obj.get_absolute_url())
     return redirect("cart:home")
