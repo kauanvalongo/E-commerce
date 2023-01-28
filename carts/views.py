1177f29c611c6f748f4eb15e65f0dc86a9f4a714
kauanvalongo <116652279+kauanvalongo@users.noreply.github.com>
2023-01-28 14:43:31 -0300

    update carts/views.py

--- carts/views.py
+++ carts/views.py
@@ -6,7 +6,7 @@ from .models import Cart
 
 def cart_home(request):
     cart_obj, new_obj = Cart.objects.new_or_get(request)
-    return render(request, "carts/home.html", {})
+    return render(request, "carts/home.html", {"cart": cart_obj})
 
 def cart_update(request):
     print(request.POST)
