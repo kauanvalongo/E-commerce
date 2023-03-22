391e5cbe00421715abb9b044e84e845b3d4e6213
kauanvalongo <116652279+kauanvalongo@users.noreply.github.com>
2023-03-22 11:02:55 -0300

    create addresses app

--- carts/views.py
+++ carts/views.py
@@ -6,6 +6,7 @@ from billing.models import BillingProfile
 from orders.models import Order
 from products.models import Product
 from .models import Cart
+from addresses.forms import AddressForm
 
 def cart_home(request):
     cart_obj, new_obj  = Cart.objects.new_or_get(request)
@@ -41,9 +42,12 @@ def checkout_home(request):
         order_obj, new_order_obj = Order.objects.get_or_create(cart = cart_obj)
     user = request.user
     billing_profile = None
+
+
     login_form = LoginForm()
     guest_form = GuestForm()
-    guest_email_id = request.session.get('guest_email_id')
+    address_form = AddressForm()
+
 
     billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
     
@@ -54,6 +58,7 @@ def checkout_home(request):
         "object": order_obj,
         "billing_profile": billing_profile,
         "login_form": login_form,
-        "guest_form": guest_form
+        "guest_form": guest_form,
+        "address_form": address_form,
     }
     return render(request, "carts/checkout.html", context)
